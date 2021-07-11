64b9a258335b24d03e9a42d0dba4afbf9ac870ae
shawnb <shawn2275@gmail.com>
2021-07-11 13:19:19 -0500

    cant get fetch to work in component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { getAllChows } from '../lib/fetch'
 
+export async function getServerSideProps(){ 
+  const res = await getAllChows()
+  const data = await res.json()
+console.log(data)
+return { props: { data }}}
 export default function Home() {
+  getAllChows()
   return (
     <div className={styles.container}>
       <Head>
@@ -11,15 +18,14 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Ciao, Chow!
         </h1>
-
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          find <em>your</em> floof!
         </p>
+        {res.map((animal) => {<p>{animal.name}</p>})}
 
-        <div className={styles.grid}>
+       <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
