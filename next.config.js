9d3f423258a24d2b6564df8694599c5549e780d1
shawnb <shawn2275@gmail.com>
2021-07-18 17:18:29 -0500

    add vercel cli and fix env vars

--- next.config.js
+++ next.config.js
@@ -2,9 +2,5 @@ module.exports = {
     images: {
         domains: ['dl5zpyw5k3jeb.cloudfront.net']
     },
-    env: {
-        API_KEY: "UqByisJkmoTnfsrwHQiGdAjvQvNnLybAO1luN3Y8cvfdOgokJF",
-API_SECRET: "u1YLsZAi3T9R4L9tbhvrLPTQHf82HgfcUa9lhEal"
-
-    }
+    
 }
