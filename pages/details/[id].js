c3ceb7817bf19f2c70bac3a07dada1f15830eb4f
shawnb <shawn2275@gmail.com>
2021-07-19 13:11:56 -0500

    add prettier and format files

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -1,21 +1,18 @@
-import { getAllChowIds, getAllChows, getChowData } from "../../lib/fetch";
-import Layout from "../../components/layout";
-import link from "next/link";
+import { getAllChowIds, getChowData } from '../../lib/fetch'
+import Layout from '../../components/layout'
 
-export default function Detail({ chowData }){
-    return <Layout>
-    {chowData.name}
-    </Layout>
+export default function Detail({ chowData }) {
+    return <Layout>{chowData.name}</Layout>
 }
 
 // this returns an array of possible values for id
 export async function getStaticPaths() {
     const paths = await getAllChowIds()
-    return { paths, fallback: false}
+    return { paths, fallback: false }
 }
 
 // this fetches the necessary data for the post with id
-export async function getStaticProps({ params }){
+export async function getStaticProps({ params }) {
     const chowData = await getChowData(params.id)
-    return{ props: { chowData }}
+    return { props: { chowData } }
 }
--- pages/index.js
+++ pages/index.js
@@ -6,51 +6,66 @@ import styles from '../styles/Home.module.css'
 import { getAllChows } from '../lib/fetch'
 
 export default function Home({ chowList }) {
-  return (
-    <Layout home>
-    <div className={styles.container}>
+    return (
+        <Layout home>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <h1 className={styles.title}>Ciao, Chow!</h1>
+                    <p className={styles.description}>
+                        find <em>your</em> floof!
+                    </p>
+                    <p>
+                        these dogs are currently available for adoption through
+                        Petfinder, all having their breed listed as Chow Chow.
+                        please note they may have Chow as a secondary breed
+                        rather than primary, so they may not look like a
+                        traditional Chow.
+                    </p>
+                    <div className={styles.grid}>
+                        {chowList.map((chow) => (
+                            <Link href={`/details/${chow.id}`} key={chow.id}>
+                                <a className={styles.card}>
+                                    {chow.primary_photo_cropped &&
+                                    chow.primary_photo_cropped.small ? (
+                                        <Image
+                                            src={
+                                                chow.primary_photo_cropped.small
+                                            }
+                                            alt={`a dog named ${chow.name}`}
+                                            width={300}
+                                            height={300}
+                                        />
+                                    ) : (
+                                        <div className={styles.placeholder}>
+                                            <p>no image available</p>
+                                        </div>
+                                    )}
+                                    <h2>{chow.name}</h2>
+                                </a>
+                            </Link>
+                        ))}
+                    </div>
+                </main>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Ciao, Chow!
-        </h1>
-        <p className={styles.description}>
-          find <em>your</em> floof! 
-        </p>
-        <p>these dogs are currently available for adoption through Petfinder, all having
-        their breed listed as Chow Chow. please note they may have Chow as a secondary 
-        breed rather than primary, so they may not look like a traditional Chow.</p>
-<div className={styles.grid}>
-        {chowList.map((chow) => 
-        <Link href={`/details/${chow.id}`} key={chow.id}><a  className={styles.card}>
-    {chow.primary_photo_cropped && chow.primary_photo_cropped.small ? 
-    <Image 
-    src={chow.primary_photo_cropped.small} 
-    width={300} height={300} /> 
-    : <div className={styles.placeholder}><p>no image available</p></div>}
-    <h2>{chow.name}</h2></a></Link>
-  )}</div>
-
-
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-    </Layout>
+                <footer className={styles.footer}>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{' '}
+                        <Image
+                            src="/vercel.svg"
+                            alt="Vercel Logo"
+                            className={styles.logo}
+                        />
+                    </a>
+                </footer>
+            </div>
+        </Layout>
     )
 }
-export async function getServerSideProps(){
-  const chowList = await getAllChows()
-  return { props: { chowList, }}
+export async function getServerSideProps() {
+    const chowList = await getAllChows()
+    return { props: { chowList } }
 }
-
-           
