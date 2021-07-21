cc615b1b8458a42da8c8b70b2cf5b8dd94a5edcc
shawnb <shawn2275@gmail.com>
2021-07-21 16:42:42 -0500

    add contact info

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -81,26 +81,36 @@ export default function Detail({ chowData }) {
                     </td>
                 </tr>
             </table>
-            <p>Would you like to learn more about {chowData.name}?</p>
+            <h3 className="mt-6 mb-4">
+                Would you like to learn more about {chowData.name}?
+            </h3>
             {chowData.contact.email ? (
-                <p className="pl-4">
+                <a
+                    className="pl-4 block mb-4"
+                    href={`mailto:{chowData.contact.email}`}
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="h-5 w-5 inline-block"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
                     >
-                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                        />
                     </svg>{' '}
                     {chowData.contact.email}
-                </p>
+                </a>
             ) : null}
             {chowData.contact.phone ? (
-                <p className="pl-4">
+                <a className="pl-4 block mb-4">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 inline-block"
+                        className="h-5 w-5 inline-block"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
@@ -113,9 +123,25 @@ export default function Detail({ chowData }) {
                         />
                     </svg>{' '}
                     {chowData.contact.phone}
-                </p>
+                </a>
             ) : null}
-            <button>View {chowData.name} on Petfinder site</button>
+            <a className="pl-4 block mb-4" href={chowData.url}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 inline-block"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                    />
+                </svg>{' '}
+                View {chowData.name} on Petfinder site
+            </a>
         </Layout>
     )
 }
