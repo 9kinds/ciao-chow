26a676baca7786496cdc41b4c225d685b4c1f3d1
shawnb <shawn2275@gmail.com>
2021-08-31 13:09:28 -0500

    fixed broken build

--- components/FancyTitle.js
+++
@@ -1,6 +0,0 @@
-export default function FancyTitle() {
-            <div className="flex flex-col">
-    <span className="text-5xl font-serif -m-5">Meet</span>{' '}
-    <h1 className="text-center mb-6 pl-6">{noShouting}!</h1>
-
-}
